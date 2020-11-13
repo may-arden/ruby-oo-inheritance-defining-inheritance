2c1e37166e2347d064a96337338d42d7e18183a9
may arden <mayard3n@gmail.com>
2020-11-13 15:15:11 -0800

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 require_relative "./vehicle.rb"
+
+# we use the < to inhert the Car class from Vehicle 
+
+class Car < Vehicle 
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+      end
+
+
+end 
